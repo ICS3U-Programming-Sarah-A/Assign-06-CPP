1ea6da9f74e1bdb8b5cd92bcc0861487662c80ba
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-06-07 15:51:29 +0000

    Not done yet

--- sorted_lists.cpp
+++ sorted_lists.cpp
@@ -8,12 +8,17 @@
 #include <iostream>
 #include <list>
 #include <string.h>
+#include <sstream>
+#include <vector>
 
-std::list<string> size;
-char sizeOfStr(std::list<string> size) {
-    std::list<string> listSize;
-    for size.size(); {
-        listSize.push_back(size.()size[counter]))
+std::list<int> sizeOfStr(std::list<std::string> size) {
+    std::list<int> listSize;
+    int temp = -1;
+    // for (int counter = 0; listSize < size; counter++) {
+    for (std::string element : size) {
+    	temp = element.size();
+        listSize.push_back(temp);
+        // push_back(size[counter]));
     }
     return listSize;
 }
@@ -23,31 +28,46 @@ int main() {
     // declare variables & list
     std::string selector;
     std::string listStr;
-    std::list<string> sizeOfString;
-    std::list<string> elementSize;
-    std::string finalList;
+    std::list<int> sizeOfString;
+    std::list<std::string> elementSize;
+    std::list<int> finalList;
+    int element;																	
     
     // intro
 	std::cout << "Today, you'll get to select whatever you'll like to complete.";
-	std::cout << "Option 1: Determining the length of the elements in a list";
-	std::cout << "Option 2: Removing duplicates in a list.";
+	std::cout << std::endl;
+	std::cout << "Option 1: Determining the length of the elements in a list.";
+	std::cout << std::endl;
+	std::cout << "Option 2: Removing duplicates in a list.\n";
 	std::cout << "Please select either or";
 	
 	// selector
 	std::cout <<  "Choose which game you'd like to execute (1, 2): ";
+	std::cout << std::endl;
 	std::cin >> selector;
 	
 	if (selector == "1") {
 	    std::cout << "Enter a list of strings (ex hi, welcome): ";
-	    std::cin >> listStr;
+	    std::getline(std::cin, listStr);
 	    
+	    for (std::string)
 	    // here's where u split (need to figure out)
 	    // here.
-	    for (string element : sizeOfString) {
+	    for (int element : listStr) {
 	        elementSize.push_back(element);
 	    }
-	    finalList = sizeOfString(elementSize);
 	    
-	    std::cout << "The length of each string is: " << finalList;
+	    finalList = sizeOfStr(elementSize);
+	    
+	    for (int sizeOfString : finalList) {
+	    	 std::cout << "The length of each string is: " << sizeOfString;
+	    }
+	  
+	} else if (selector == "2") {
+		std::string userStr;
+		// get user input
+		std::cout << "Enter a list of elements: ";
+		std::getline(std::cin, userStr);
+		
 	}
 }
